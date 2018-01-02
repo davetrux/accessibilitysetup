c6205d45da7eebdea09cc4fe10f3fd91398ff3bb
David Truxall <david.truxall@hpe.com>
2018-01-02 17:02:04 -0500

    Initial commit

---
+++ app/src/main/java/com/truxall/accessibilitytest/MainActivity.java
@@ -0,0 +1,176 @@
+package com.truxall.accessibilitytest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ThemeUtils.setUiTheme(this);
+
+        setContentView(R.layout.activity_main);
+        // Set up the login form.
+        mEmailView = findViewById(R.id.email);
+
+        mPasswordView = findViewById(R.id.password);
+
+        // Accessibility feature:
+        //
+        // Handle enter key as a login action
+        mPasswordView.setOnEditorActionListener((textView, id, keyEvent) -> {
+            if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                attemptLogin();
+                return true;
+            }
+            return false;
+        });
+
+        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(view -> attemptLogin());
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password) ||  !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            /*
+             * Accessibility Feature
+             */
+            mPasswordView.announceForAccessibility(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            /*
+             * Accessibility Feature
+             */
+            mEmailView.announceForAccessibility(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            /*
+             * Accessibility Feature
+             */
+            mEmailView.announceForAccessibility(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+
+            // Hide the keyboard
+            InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            if(inputManager != null) {
+                inputManager.hideSoftInputFromWindow(mPasswordView.getWindowToken(), 0);
+            }
+
+            // Attempt a (fake) login
+            loginObservable(email, password)
+                    .subscribeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe(__ -> showProgress(true))
+                    .subscribe(success -> {
+                        if(!success) {
+                            showProgress(false);
+                            mPasswordView.setError(getString(R.string.error_incorrect_password));
+                            mPasswordView.announceForAccessibility(getString(R.string.error_incorrect_password));
+                            mPasswordView.requestFocus();
+                            if(inputManager != null) {
+                                inputManager.showSoftInput(mPasswordView, 0);
+                            }
+                        } else {
+                            finish();
+                        }
+                    });
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() >= 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    private void showProgress(final boolean show) {
+
+        if(show) {
+            mProgressView.setVisibility(View.VISIBLE);
+            mLoginFormView.setVisibility(View.GONE);
+        } else {
+            mProgressView.setVisibility(View.GONE);
+            mLoginFormView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    private Observable<Boolean> loginObservable(String email, String password) {
+        return Observable.fromCallable(() -> {
+            // Simulate network access.
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+            // 5 or 10 letter passwords fail (for demonstration purposes)
+            return password.length() % 5 == 0;
+        });
+    }
+}
